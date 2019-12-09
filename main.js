d45a7d995387adfe3d4964d1cf544a3ebad61b6c
hmisonoo <41407535+hmisonoo@users.noreply.github.com>
2019-12-09 16:50:20 +0900

    Update main.js

--- main.js
+++ main.js
@@ -53,7 +53,7 @@ function math(){
 	}else{
 		let count = winner.length + 1;
 		text1.textContent = random;
-		text2.textContent = "当選者　" + count + "名";
+		text2.textContent = "当選者　" + count + "名／+　max + 1 +名;
 		console.log(random);
 		const tbody = document.getElementById('tbodyId');
 		const newRow = tbody.insertRow();
