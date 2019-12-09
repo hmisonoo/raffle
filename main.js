e54f42a52e2c2ce8ee1dcaf4a0171fbdf6bc7311
hmisonoo <41407535+hmisonoo@users.noreply.github.com>
2019-12-09 17:05:00 +0900

    Update main.js

--- main.js
+++ main.js
@@ -36,6 +36,7 @@ download_btn.addEventListener('click',downloadCsv)
 function math(){
 	let min = parseInt(document.getElementById('input1').value,10);
 	let max = parseInt(document.getElementById('input2').value,10);
+	let persons = max - min;
 	if(min > max || !min || !max){
 		alert('範囲の指定に誤りがあります');
 		return;
@@ -53,7 +54,7 @@ function math(){
 	}else{
 		let count = winner.length + 1;
 		text1.textContent = random;
-		text2.textContent = "当選者　" + count + "　／　"+　max - min + "名";
+		text2.textContent = "当選者　" + count + "　／　"+　persons + "名";
 		console.log(random);
 		const tbody = document.getElementById('tbodyId');
 		const newRow = tbody.insertRow();
